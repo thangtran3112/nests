c7e48b0aaaa346f3b6fede73e73511ca266b300f
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-06-19 23:04:23 -0700

    add api

--- cdk-rest-webpack/bin/cdk-graphql-rest.ts
+++ cdk-rest-webpack/bin/cdk-graphql-rest.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { RestStack } from '../lib/rest-stack';
+import { RestStack } from '../lib/graphql-rest-stack';
 
 const app = new cdk.App();
 new RestStack(app, 'WebpackNestStack', {
