dd40e0960e977ad8e85b0520a2e1bdc6bde10ae7
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-06-20 00:06:55 -0700

    feat: fixed webpack and enabled nestjs/mapped-types

---
+++ cdk-rest-webpack/api-rest/src/books/books.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+
+@Injectable()
+export class BooksService {
+  create(createBookDto: CreateBookDto) {
+    return { name: createBookDto.name, id: createBookDto.id };
+  }
+
+  findAll() {
+    return { name: 'Superman', id: 'Superman' };
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} book`;
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    return `This action updates a #${id} book`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
