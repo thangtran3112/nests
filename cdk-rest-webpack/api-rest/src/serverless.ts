57509572c08b51b464e4086ddd0f0c469ad7efe0
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-06-19 23:21:28 -0700

    add api rest

---
+++ cdk-rest-webpack/api-rest/src/serverless.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+import { AppModule } from './app.module';
+
+let cachedServer: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
+
+/**
+ * This handler is only to be built for Lambda purpose
+ * For Typescript tsc build, the entry point is main.ts
+ */
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  cachedServer = cachedServer ?? (await bootstrap());
+  return cachedServer(event, context, callback);
+};
